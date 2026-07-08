c0777f1bd4eb85d768b1879d2da19af8fcd575b3
danila <danilagorshunov@gmail.com>
2026-07-08 14:03:59 +0300

    Enable polling for weather data with a 30-second interval

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,9 +1,12 @@
 import {useAppSelector} from "../app/hooks.ts";
 import {useGetWeatherByCityQuery} from "../features/api/weatherApi.ts";
 
+
 const Weather = () => {
     const city = useAppSelector(state => state.city);
-    const {data, error, isLoading} = useGetWeatherByCityQuery(city);
+    const {data, error, isLoading} = useGetWeatherByCityQuery(city, {
+        pollingInterval: 30000,
+    });
 
     if (!city) {
         return <div className={'infoWeath'}>Enter city name</div>
